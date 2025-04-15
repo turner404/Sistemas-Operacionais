76696f527ef10ade6c846e48094304490262b575
Luiz Guilherme Geron Manfrim Coelho <77393049+LuizGulhermeGeron@users.noreply.github.com>
2025-04-15 00:50:31 -0300

    Update pstree.cpp

--- Laboratorio_02/pstree.cpp
+++ Laboratorio_02/pstree.cpp
@@ -1,39 +1,54 @@
-// Descrição: Programa que cria uma árvore de processos de acordo com a profundidade informada pelo usuário.
+// Descrição: Programa que cria uma árvore de processos.
 // Autor: Rafael Augusto de Souza
-// Data: 2025-04-09
+// Data: 2025-04-14
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <cstdlib>
+#include <string.h>
 
-using namespace std;
-void criaArvore(int nivel, int max) {
-    if (nivel >= max) 
-        return; 
-
-    pid_t pid1 = fork();
-
-    if (pid1 == 0) {
-        criaArvore(nivel + 1, max);
-        return;
+int main(int argc, char** argv) {
+    printf("Exemplo 1:\n");
+    
+    int numNiveis = 3;         // Número de níveis de processos
+    int duracaoSegundos = 20;  // Tempo de duração do processo (em segundos)
+    int numFilhos = 2;         // Número de filhos por processo
+    
+    // Lê os argumentos passados pela linha de comando
+    for (int i = 0; i < argc; i++) {
+        if (!strcmp(argv[i], "-n")) {
+            sscanf(argv[i + 1], "%d", &numNiveis);
+        }
+        if (!strcmp(argv[i], "-t")) {
+            sscanf(argv[i + 1], "%d", &duracaoSegundos);
+        }
+        if (!strcmp(argv[i], "-c")) {
+            sscanf(argv[i + 1], "%d", &numFilhos);
+        }
     }
-    pid_t pid2 = fork();
-
-    if (pid2 == 0) {
-        criaArvore(nivel + 1, max);
-        return;
+    
+    printf("Número de níveis (-n) = %d\n", numNiveis);
+    printf("Duração (em segundos) (-t) = %d\n", duracaoSegundos);
+    printf("Número de filhos (-c) = %d\n", numFilhos);
+    
+    printf("Meu PID = %d\n", getpid());
+    printf("PID do meu pai = %d\n", getppid());
+    
+    int nivelAtual = 1;
+    pid_t pid;
+    
+    for (int i = 0; i < numFilhos; i++) {
+        pid = fork();
+        if (pid == 0) {  // Processo filho
+            nivelAtual++;
+            if (nivelAtual < numNiveis) {
+                i = -1; // Reinicia o loop para criar filhos no próximo nível
+            } else {
+                break;  // Último nível, para de criar filhos
+            }
+        }
     }
-    wait(nullptr);
-    wait(nullptr);
-}
-
-int main(int argc, char *argv[]) {
-    int N = atoi(argv[1]);
-    cout << "PID da raiz: " << getpid() << "\n";
-
-    criaArvore(1, N);
-    sleep(30);
-
-    return;
+    
+    sleep(duracaoSegundos);
 }
