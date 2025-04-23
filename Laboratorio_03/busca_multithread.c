66946d0f5059502bc627f25e244bf1fa3878e6ab
Victórya <72286081+turner404@users.noreply.github.com>
2025-04-22 23:38:09 -0300

    Update busca_multithread.c

--- Laboratorio_03/busca_multithread.c
+++ Laboratorio_03/busca_multithread.c
@@ -1,6 +1,6 @@
-// Descrição: Programa que divide um array em N partes e cria N threads para procurar um valor em cada parte
-// Autor: Victórya Carolina Guimarães da Luz
-// Data: 16-04-2025
+// Descrição: Um programa com N threads que localiza um valor em um vetor de inteiros, sendo que o espaço de busca no vetor é distribuído entre as N threads.
+// Autor: Rafael Augusto de Souza.
+// Data: 21-04-2025.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,79 +14,19 @@ typedef struct ThreadData {
     int searchValue;
 } ThreadData;
 
-// Funções
-int* generateArray(int size);
-void printArray(int size, int* array);
-void* searchValueFn(void* data);
-ThreadData* createStruct();
+volatile int found = 0;
+int found_index = -1;
+pthread_mutex_t mtx;
 
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        printf("Uso: %s <tamanho do vetor> <quantidade_threads>\n", argv[0]);
-        return 1;
-    }
-
-    int searchValue;
-    int threadCount = atoi(argv[2]);
-    int arraySize = atoi(argv[1]);
-
-    srand(time(NULL)); // Inicializa semente para números aleatórios
-    int* array = generateArray(arraySize);
-    printArray(arraySize, array);
-
-    printf("Digite o valor a ser procurado: ");
-    scanf("%d", &searchValue);
-
-    int sizePartPerThread;
-    if (arraySize > threadCount) {
-        sizePartPerThread = arraySize / threadCount;
-    } else {
-        sizePartPerThread = 1;
-        threadCount = arraySize;
-    }
-
-    // Vetor para guardar os IDs das threads
-    pthread_t* threads = (pthread_t*) malloc(threadCount * sizeof(pthread_t));
-
-    for (int i = 0; i < threadCount; i++) {
-        ThreadData* data = createStruct();
-
-        int start = i * sizePartPerThread;
-        int end = (i == threadCount - 1) ? arraySize - 1 : (start + sizePartPerThread - 1);
-
-        data->array = array;
-        data->start = start;
-        data->end = end;
-        data->searchValue = searchValue;
-
-        pthread_create(&threads[i], NULL, searchValueFn, (void*) data);
-    }
-
-    // Aguarda todas as threads terminarem
-    for (int i = 0; i < threadCount; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    free(array);
-    free(threads);
-    return 0;
-}
-
-// Função que aloca memória para a struct ThreadData.
-ThreadData* createStruct() {
-    return (ThreadData*) malloc(sizeof(ThreadData));
-}
-
-// Função que gera um vetor de inteiros com valores aleatórios.
+// Gerando o array de inteiros
 int* generateArray(int size) {
-    int* array = (int*) malloc(size * sizeof(int));
+    int* array = malloc(size * sizeof(int));
     for (int i = 0; i < size; i++) {
         array[i] = rand() % 10;
     }
     return array;
 }
 
-// Função que imprime o vetor.
 void printArray(int size, int* array) {
     printf("Array: [");
     for (int i = 0; i < size; i++) {
@@ -96,22 +36,85 @@ void printArray(int size, int* array) {
     printf("]\n");
 }
 
-// Função que procura um valor no vetor.
 void* searchValueFn(void* data) {
-    ThreadData* threadData = (ThreadData*) data;
-    int foundValue = 0;
+    ThreadData* d = (ThreadData*) data;
+    for (int i = d->start; i < d->end; i++) {
+        pthread_mutex_lock(&mtx);
+        if (found) {
+            pthread_mutex_unlock(&mtx);
+            break;
+        }
+        pthread_mutex_unlock(&mtx);
 
-    for (int i = threadData->start; i <= threadData->end; i++) {
-        if (threadData->array[i] == threadData->searchValue) {
-            printf("Valor encontrado na posição %d - thread [%d...%d]\n", i, threadData->start, threadData->end);
-            foundValue = 1;
+        if (d->array[i] == d->searchValue) {
+            pthread_mutex_lock(&mtx);
+            if (!found) {
+                found = 1;
+                found_index = i;
+                printf("Valor %d encontrado na posição %d pela thread [%d...%d)\n", d->searchValue, i, d->start, d->end);
+            }
+            pthread_mutex_unlock(&mtx);
+            break;
         }
     }
 
-    if (!foundValue) {
-        printf("Valor não encontrado - thread [%d...%d]\n", threadData->start, threadData->end);
+    if (!found) {
+        printf("Valor não encontrado na thread [%d...%d)\n", d->start, d->end);
     }
 
-    free(threadData);
-    pthread_exit(NULL);
+    free(d);
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        printf("Uso: %s <tamanho do vetor> <quantidade_threads>\n", argv[0]);
+        return 1;
+    }
+
+    int arraySize = atoi(argv[1]);
+    int threadCount = atoi(argv[2]);
+    int searchValue;
+
+    srand(time(NULL));
+    int* array = generateArray(arraySize);
+    printArray(arraySize, array);
+
+    printf("Digite o valor a ser procurado: ");
+    scanf("%d", &searchValue);
+
+    if (arraySize < threadCount) {
+        threadCount = arraySize;
+    }
+
+    pthread_t* threads = malloc(threadCount * sizeof(pthread_t));
+    pthread_mutex_init(&mtx, NULL);
+
+    int chunk = (arraySize + threadCount - 1) / threadCount;
+
+    for (int i = 0; i < threadCount; i++) {
+        ThreadData* data = malloc(sizeof(ThreadData));
+        data->array = array;
+        data->searchValue = searchValue;
+        data->start = i * chunk;
+        data->end = (data->start + chunk > arraySize) ? arraySize : data->start + chunk;
+
+        if (pthread_create(&threads[i], NULL, searchValueFn, data) != 0) {
+            perror("Erro ao criar thread");
+            exit(1);
+        }
+    }
+
+    for (int i = 0; i < threadCount; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    if (!found) {
+        printf("Valor %d não encontrado no array.\n", searchValue);
+    }
+
+    pthread_mutex_destroy(&mtx);
+    free(array);
+    free(threads);
+    return 0;
 }
